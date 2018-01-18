fe0fcfac3c1aa061c03c8bd457f6cab70957e225
Furyzer0 <34964592+Furyzer0@users.noreply.github.com>
2018-01-18 23:34:00 +0300

    Add files via upload

---
+++ C#/Search/Pair.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static int pairs(int k, int[] arr) {
+        int count = 0; 
+        Array.Sort(arr);
+        int max = arr.Length - 1;
+
+        for(int i = 0; i < arr.Length; i++)
+        {
+            int min = i;
+            while(min <= max)
+            {
+                int mid = (min + max) / 2;
+
+                if(arr[i] + k == arr[mid])
+                {
+                    count++;
+                    break;
+                }
+                else if(arr[i] + k < arr[mid])
+                    max = mid - 1;
+                else
+                    min = mid + 1;
+            }
+        }
+
+        return count;
+    }
+
+    static void Main(String[] args) {
+        string[] tokens_n = Console.ReadLine().Split(' ');
+        int n = Convert.ToInt32(tokens_n[0]);
+        int k = Convert.ToInt32(tokens_n[1]);
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arr_temp,Int32.Parse);
+        int result = pairs(k, arr);
+        Console.WriteLine(result);
+    }
+}
