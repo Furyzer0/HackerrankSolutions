77ba7e7d570b0812305c106fdcc754afcbfe8b90
Furyzer0 <34964592+Furyzer0@users.noreply.github.com>
2018-01-21 10:39:06 +0300

    Not necessary to find end coordinates

--- C++/Search/SolveMaze.cpp
+++ C++/Search/SolveMaze.cpp
@@ -5,8 +5,8 @@
 
 using namespace std;
 
-string countLuck(vector <string> matrix, int length);
-bool solveMaze(vector <string> matrix, int length, int k, int x, int y, int endx, int endy, int count);
+void countLuck(vector <string> matrix, int length);
+bool solveMaze(vector <string> matrix, int length, int k, int x, int y, int count);
 bool using_wand(vector <string> matrix, int length, int x, int y);
 
 int main() {
@@ -20,40 +20,32 @@ int main() {
         for(int matrix_i = 0; matrix_i < n; matrix_i++){
            cin >> matrix[matrix_i];
         }
-        string result = countLuck(matrix, n);
-        cout << result << endl;
+        countLuck(matrix, n);
     }
     return 0;
 }
 
-string countLuck(vector <string> matrix, int length) {
+void countLuck(vector <string> matrix, int length) {
     int k;
     cin >> k;
-    int startx = -1, starty = -1, endx = -1, endy = -1;
+    int startx = -1, starty = -1;
     for(int i = 0; i < length; i++){
         for(int j = 0; j < matrix[0].length(); j++){
             char current = matrix[i][j];
-            if(current == '*'){
-                endx = j;
-                endy = i;
-                if(startx != -1)
-                    break;
-            }
-            else if(current == 'M'){
+            if(current == 'M'){
                 startx = j;
                 starty = i;
-                if(endx != -1)
-                    break;
+                break;
             }
         }
     }
     //cout << startx << ", " << starty << ", " << endx << ", " << endy << ", " << endl;
-    solveMaze(matrix, length, k, startx, starty, endx, endy, 0);
-    return "";
+    solveMaze(matrix, length, k, startx, starty, 0);
+    return;
 }
 
-bool solveMaze(vector <string> matrix, int length, int k, int x, int y, int endx, int endy, int count){
-    if(x == endx && y == endy){
+bool solveMaze(vector <string> matrix, int length, int k, int x, int y, int count){
+    if(matrix[y][x] == '*'){
         for(int i = 0; i < length; i++){
             cout << matrix[i] << endl;
         }      
@@ -71,19 +63,19 @@ bool solveMaze(vector <string> matrix, int length, int k, int x, int y, int endx
         count++;
     }
     if(x != 0){
-        if(solveMaze(matrix, length, k, x - 1, y, endx, endy, count))
+        if(solveMaze(matrix, length, k, x - 1, y, count))
             return true;
     }  
     if(x != matrix[0].length() - 1){
-        if(solveMaze(matrix, length, k, x + 1, y, endx, endy, count))
+        if(solveMaze(matrix, length, k, x + 1, y, count))
             return true;
     }
     if(y != 0){
-        if(solveMaze(matrix, length, k, x, y - 1, endx, endy, count))
+        if(solveMaze(matrix, length, k, x, y - 1, count))
             return true;
     }
     if(y != length - 1){
-        if(solveMaze(matrix, length, k, x, y + 1, endx, endy, count))
+        if(solveMaze(matrix, length, k, x, y + 1, count))
             return true;
     } 
     return false;
