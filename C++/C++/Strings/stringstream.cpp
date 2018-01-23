aa1f6a99fd7ed18ecf0b14685d31f88751bb8f35
Furyzer0 <34964592+Furyzer0@users.noreply.github.com>
2018-01-23 12:37:34 +0300

    stringstream challenge added

---
+++ C++/C++/Strings/stringstream.cpp
@@ -0,0 +1,27 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+    stringstream ss(str);
+    vector<int> result(0);
+    int i;
+    char ch;
+    while(ss >> i){
+        result.push_back(i);
+        ss >> ch;
+    }
+    return result;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
